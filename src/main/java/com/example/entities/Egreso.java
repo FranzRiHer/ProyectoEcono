8ca5dff4864f61c07d2a4a5c074d0019c7ce012d
juanito <juan.sanabria03@usa.edu.co>
2024-03-20 18:35:14 -0500

    Se creo la relacion ManyToOne con la clase Usuario para tener control de los gastos que realiza cada usuario, se elimino un atributo nombre de la clase gasto que no se iba a utilizar con sus getters y setters

--- src/main/java/com/example/entities/Egreso.java
+++ src/main/java/com/example/entities/Egreso.java
@@ -1,28 +1,32 @@
 package com.example.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
-public class Egresos {
+public class Egreso {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idEgreso; 
-    private String nombreEgreso; 
     private int cantidadEgreso;
     private String descripcion;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(id = "id_usuario")
+    private Usuario usuario;
+
 
     public Long getIdEgreso() {
         return idEgreso;
     }
     public void setIdEgreso(Long idEgreso) {
         this.idEgreso = idEgreso;
-    }
-    public String getNombreEgreso() {
-        return nombreEgreso;
-    }
-    public void setNombreEgreso(String nombreEgreso) {
-        this.nombreEgreso = nombreEgreso;
     }
     public int getCantidadEgreso() {
         return cantidadEgreso;
