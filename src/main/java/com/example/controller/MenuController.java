9cd8c7cc6e4021284e72515d58df305ce831d9ac
JohanSebOliveros <johan.oliveros01@usa.edu.co>
2024-03-20 22:01:37 -0500

    Finalizacion, back menu

--- src/main/java/com/example/controller/MenuController.java
+++ src/main/java/com/example/controller/MenuController.java
@@ -4,12 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entities.Menu;
-
-
-
+import com.example.service.MenuService;
 
 @RestController
 @RequestMapping("/menu")
@@ -17,13 +16,13 @@ public class MenuController {
     @Autowired
     private MenuService menuService;
 
-    @GetMapping("/menu/{id}")
-    public Menu getView(@PathVariable String id) {
+    @GetMapping("/vista/{id}")
+    @ResponseBody // Esto hace que Spring MVC trate la respuesta como cuerpo de la respuesta
+    public String getView(@PathVariable String id) {
         Menu menu = menuService.getView(id);
         if (menu != null) {
-            return menu;
+            return menu.getUrl();
         }
-        return null;
+        return "Error: recurso no encontrado"; // O podrías lanzar una excepción personalizada aquí.
     }
-
 }
