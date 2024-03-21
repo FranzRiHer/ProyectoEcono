3f54cfe631c19150de693ca53a0f7c32ce409a0a
NataliaRincon <karen.rincon01@usa.edu.co>
2024-03-21 01:51:59 -0500

    Correccion vista principal

--- src/main/resources/static/principal/js/script.js
+++ src/main/resources/static/principal/js/script.js
@@ -1,3 +1,31 @@
+function redireccionar(elemento) {
+    var idElemento = elemento.id;
+    sendLink(idElemento, function (nuevoLink) {
+        if (nuevoLink) {
+            window.location.href = nuevoLink; // Redirige a la nueva URL
+        } else {
+            console.error("No se pudo obtener el nuevo enlace.");
+        }
+    });
+}
+
+function sendLink(idElemento, callback) {
+    $.ajax({
+        url: "http://localhost:8080/menu/vista/" + idElemento,
+        type: "GET",
+        dataType: "text", // Cambiado a "text" porque estamos esperando una ruta como texto
+
+        success: function (result) {
+            console.log(result);
+            callback(result);
+        },
+        error: function (error) {
+            console.log(error);
+            callback(null);
+        },
+    });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     populateUl();
     getIngresos();
@@ -77,8 +105,3 @@ const populateUl = () => {
         ul.appendChild(li);
     });
 };
-
-
-
-
-
