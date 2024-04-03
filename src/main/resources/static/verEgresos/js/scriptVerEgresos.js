bae92f14376dc7f932460e309ce20e348f963854
MateoGutSua <mateo.gutierrez01@usa.edu.co>
2024-04-03 16:59:03 -0500

    logica para recuperar y mostrar los datos de gastos almacenados en la base de datos

---
+++ src/main/resources/static/verEgresos/js/scriptVerEgresos.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', function() {
+  getEgresos();
+});
+
+
+function getEgresos() {
+  let token = localStorage.getItem('token');
+  // URL para la peticion
+  var url = "http://localhost:8080/gastos/egresos";
+
+  // Realizar solicitud GET usando Fetch API
+  fetch(url, {
+    method: 'GET', // o 'POST', 'PUT', 'DELETE', etc., según sea necesario
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      // Agrega cualquier otro encabezado requerido aquí
+    },
+  }
+  )
+    .then(response => response.json()) // Convertir respuesta a JSON
+    .then(data => {
+      // Manipular los datos recibidos
+      mostrarDatosEnTabla(data);
+    })
+    .catch(error => {
+      console.error("Error al obtener los datos:", error);
+    });
+}
+
+function mostrarDatosEnTabla(data) {
+  var tabla = document.getElementById("tablaDatos");
+  tabla.innerHTML = ""; // Limpiar tabla antes de agregar nuevos datos
+
+  // Agregar encabezados
+  var thead = document.createElement("thead");
+  var filaEncabezado = document.createElement("tr");
+  var thCantidad = document.createElement("th");
+  var thDescripcion = document.createElement("th");
+
+  thCantidad.textContent = "Cantidad";
+  thDescripcion.textContent = "Descripción";
+
+  filaEncabezado.appendChild(thCantidad);
+  filaEncabezado.appendChild(thDescripcion);
+  thead.appendChild(filaEncabezado);
+
+  tabla.appendChild(thead);
+
+  // Iterar sobre los datos y crear filas de tabla
+  data.forEach(rowData => {
+    var row = tabla.insertRow();
+
+    // Accede directamente a las propiedades 'cantidad' y 'descripcion'
+    var cellCantidad = row.insertCell();
+    cellCantidad.textContent = rowData.cantidad; // Primero 'cantidad'
+
+    var cellDescripcion = row.insertCell();
+    cellDescripcion.textContent = rowData.descripcion; // Luego 'descripcion'
+  });
+  // Fijar encabezados
+  thead.style.position = "sticky";
+  thead.style.top = "0";
+}
