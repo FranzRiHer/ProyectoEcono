6841f0a8f17c431a39bbc0958f1f2911a736864c
FranzRincon <franzrihe@gmail.com>
2024-04-03 15:32:45 -0500

    Merge FranzSP2

--- src/main/java/com/example/controller/UsuarioController.java
+++ src/main/java/com/example/controller/UsuarioController.java
@@ -1,7 +1,6 @@
 package com.example.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.entities.Usuario;
