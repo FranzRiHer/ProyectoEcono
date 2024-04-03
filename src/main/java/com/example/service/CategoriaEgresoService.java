13480a57e9674bcd6c88919e43ff6c12ed10abe5
FranzRincon <franzrihe@gmail.com>
2024-04-03 01:04:30 -0500

    Commit front end categorias

--- src/main/java/com/example/service/CategoriaEgresoService.java
+++ src/main/java/com/example/service/CategoriaEgresoService.java
@@ -1,6 +1,5 @@
 package com.example.service;
 
-import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,22 +19,5 @@ public class CategoriaEgresoService {
     @Transactional
     public CategoriaEgreso save(CategoriaEgreso catE){
         return catEgRepo.save(catE);
-    }
-
-    public List<CategoriaEgreso> categoriasEgresosIniciales(Long idUsuario) {
-        // Verificar si existen categorías para el número de usuario
-        List<CategoriaEgreso> categoriasExistente = catEgRepo.getCategorasEgresosUsuario(idUsuario);
-        if (categoriasExistente.isEmpty()) {
-            // Crear y guardar las categorías predeterminadas
-            CategoriaEgreso categoria1 = new CategoriaEgreso( "Comida", idUsuario);
-            CategoriaEgreso categoria2 = new CategoriaEgreso( "Social", idUsuario);
-            CategoriaEgreso categoria3 = new CategoriaEgreso( "Transporte", idUsuario);
-            List<CategoriaEgreso> categoriasNuevas = Arrays.asList(categoria1, categoria2, categoria3);
-            catEgRepo.save(categoria1);
-            catEgRepo.save(categoria2);
-            catEgRepo.save(categoria2);
-            return categoriasNuevas;
-        }
-        return categoriasExistente;
     }
 }
--- src/main/resources/static/registroGastos/js/scriptGastos.js
+++ src/main/resources/static/registroGastos/js/scriptGastos.js
@@ -1,9 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    cargarCategorias();
+});
+
 function mostrarPersonalizado(){
     var opcion = document.getElementById("categoriaMenu");
     if(opcion.value === "personalizado") {
-        document.getElementById('opcionPersonalizada').style.display = 'block';
+        document.getElementById('categoriaPersonalizada').style.display = 'block';
+        document.getElementById('validadCatBTN').style.display = 'block';
     } else {
-        document.getElementById('opcionPersonalizada').style.display = 'none';
+        document.getElementById('categoriaPersonalizada').style.display = 'none';
+        document.getElementById('validadCatBTN').style.display = 'none';
     }
 }
 
@@ -16,6 +22,56 @@ function validarLabels() {
     } else {
         window.alert("Los campos deben estar llenos y el valor debe ser numérico."); // El contenido no es un número o está vacío
     }
+}
+
+function validarCategoria(){
+    let descripcion = $("#categoriaPersonalizada").val();
+    document.getElementById('categoriaPersonalizada').value = '';
+
+    if((descripcion !== "")){
+        crearCategoria(descripcion)
+    } else {
+        window.alert("El campo de categoria debe estar lleno.");
+    }
+}
+
+function crearCategoria(desc){
+    let userid = localStorage.getItem('userId');
+    let data = {
+        descripcion: desc,
+        id_usuario : userid
+    }
+
+    let dataToSend = JSON.stringify(data);
+    console.log(dataToSend);
+
+    let token = localStorage.getItem('token');
+
+    $.ajax({
+        url: "http://localhost:8080/categorias_e/add_cat_e",
+        type: "POST",
+        dataType: "JSON",
+        contentType: "application/json; charset=utf-8",
+        data: dataToSend,
+        // Incluir el token de autenticación en el encabezado de la solicitud
+        beforeSend: function(xhr) {
+            if (token) {
+                xhr.setRequestHeader("Authorization", "Bearer " + token);
+            }
+        },
+        success: function (result) {
+            console.log(result);
+            $("#egresoInput").val("");
+            $("#descripcionEgreso").val("");
+            console.log(result);
+            alert('Datos enviados exitosamente.');
+        },
+        error: function (error) {
+            console.log(error);
+            alert('Error al enviar los datos. Por favor, inténtelo de nuevo.');
+        }
+    });
+
 }
 
 function saveEgreso(egreso, descripcion) {
@@ -44,8 +100,7 @@ function saveEgreso(egreso, descripcion) {
         },
         success: function (result) {
             console.log(result);
-            $("#egresoInput").val("");
-            $("#descripcionEgreso").val("");
+            $("#categoriaPersonalizada").val("");
             console.log(result);
             alert('Datos enviados exitosamente.');
         },
@@ -56,3 +111,47 @@ function saveEgreso(egreso, descripcion) {
     });
 }
 
+function cargarCategorias() {
+    let userid = localStorage.getItem('userId');
+    let token = localStorage.getItem('token');
+
+    $.ajax({
+        url: "http://localhost:8080/categorias_e/get_cat_e_user/" + userid,
+        type: "GET",
+        dataType: "JSON",
+        beforeSend: function(xhr) {
+            if (token) {
+                xhr.setRequestHeader("Authorization", "Bearer " + token);
+            }
+        },
+        success: function (result) {
+            const select = $('#categoriaMenu');
+            select.empty();
+
+            // Verificar si el resultado está vacío
+            if (result.length === 0) {
+
+                // Categorías por defecto
+                setTimeout(() => crearCategoria('Comida'), 0);
+                setTimeout(() => crearCategoria('Transporte'), 500);
+                setTimeout(() => crearCategoria('Social'), 1000);
+
+            } else {
+                // Cargar categorías del resultado
+                $.each(result, function() {
+                    const option = $('<option>', { value: this.idCategoriaEgreso, text: this.descripcion });
+                    select.append(option);
+                });
+            }
+
+            // Añadir opción personalizada al final
+            const optionPersonalizado = $('<option>').attr('value', 'personalizado').text('Otro...');
+            select.append(optionPersonalizado);
+        },
+        error: function(xhr, status, error) {
+            console.error('Error al cargar las categorías:', error);
+        }
+    });
+}
+
+setInterval(cargarCategorias, 10000);
