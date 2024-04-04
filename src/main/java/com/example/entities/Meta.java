85f0eba73d69212bf3406f1ac9938ff1a1c84a65
Gabriel Montiel <gabomonher20@gmail.com>
2024-04-03 23:51:27 -0500

    Creacion clase meta y la vista lista de configuracion meta

---
+++ src/main/java/com/example/entities/Meta.java
@@ -0,0 +1,62 @@
+package com.example.entities;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+
+@Entity
+public class Meta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private double porcentaje;
+    @JsonIgnore
+    private int total = 0;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPorcentaje() {
+        return porcentaje;
+    }
+
+    public void setPorcentaje(double porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+    
+
+}
---
+++ src/main/resources/static/metaConfiguracion/js/scriptMetaConfig.js
@@ -0,0 +1,76 @@
+function getCliente() {
+    let id = localStorage.getItem('userId') // Asegúrate de obtener el valor correcto
+    console.log("ID del usuario: ", id); // Verificar el ID obtenido
+    let token = localStorage.getItem("token");
+    var url = "http://localhost:8080/usuarios/usuario_get/" + id;
+  
+    fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+      setMeta(data); // Pasar el objeto directamente
+    })
+    .catch((error) => {
+      console.error("Error al obtener los datos:", error);
+    });
+  }
+
+function setMeta(data) {
+    var nombre = "lujos";
+    var porcentaje_lujos = document.getElementById("lujos").value;
+    porcentaje_lujos = porcentaje_lujos / 100;
+
+    // Validar si la cifra de dinero es un número
+    if (!isNaN(porcentaje_lujos)) {
+        let token = localStorage.getItem('token');
+        let id = localStorage.getItem('userId')
+        // Crear objeto JSON con los datos
+        var datos = {
+            nombre: nombre,
+            porcentaje: porcentaje_lujos,
+            usuario: data
+        };
+        console.log(datos)
+        // Configurar opciones para la solicitud fetch
+        var opciones = {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(datos)
+
+        };
+        console.log(JSON.stringify(datos))
+
+        // URL a la que se enviarán los datos
+        var url = 'http://localhost:8080/metas/add/' + id;
+        console.log(url)
+        // Realizar solicitud fetch
+        fetch(url, opciones)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al enviar los datos.');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Manejar la respuesta si es necesario
+            console.log('Respuesta del servidor:', data);
+            alert('Datos enviados exitosamente.');
+            $("#lujos").val("");
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Error al enviar los datos. Por favor, inténtelo de nuevo.');
+        });
+    } else {
+        // Mostrar alerta si la cifra de dinero no es un número
+        alert("La cifra de dinero debe ser un número.");
+    }
+}
