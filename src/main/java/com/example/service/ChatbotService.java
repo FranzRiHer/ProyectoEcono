1fa7a6ce0623668ee9b598bc115848cc4f44e5b1
Gabriel Montiel <gabomonher2.0@gmail.com>
2024-04-15 17:43:15 -0500

    Prueba Piloto modificada para hacer las peticiones desde el backend para mayor seguridad con los tokens

--- src/main/java/com/example/service/ChatbotService.java
+++ src/main/java/com/example/service/ChatbotService.java
@@ -1,5 +1,6 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -11,10 +12,15 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class ChatbotService {
 
+    @Value("${chatbot.token}")
+    private String chatbotToken;
+
+
     private RestTemplate restTemplate = new RestTemplate();
 
     public String sendQueryToChatbot(String userMessage) {
         HttpHeaders headers = new HttpHeaders();
+        System.out.println(userMessage);
         headers.setBearerAuth("68ace983-7f02-4dac-aeae-11c568d0f9e2"); // Pon tu token aquí
         headers.setContentType(MediaType.APPLICATION_JSON);
 
@@ -26,7 +32,7 @@ public class ChatbotService {
 
         // Envía la solicitud POST
         ResponseEntity<String> response = restTemplate.exchange(
-            "https://www.stack-inference.com/inference/v0/run/5f88e2a7-998b-4cca-a223-efff3e8e325a/661c4068895f652a7f53219f", // URL de la API del chatbot
+            "https://api.stack-ai.com/inference/v0/run/5f88e2a7-998b-4cca-a223-efff3e8e325a/661c4068895f652a7f53219f", // URL de la API del chatbot
             HttpMethod.POST,
             entity,
             String.class
