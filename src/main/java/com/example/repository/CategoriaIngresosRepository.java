6bab252bc296116bb9da254ec1ab9d4e7e470f23
FranzRincon <franzrihe@gmail.com>
2024-04-15 20:45:54 -0500

    Backend categorias Ingresos

---
+++ src/main/java/com/example/repository/CategoriaIngresosRepository.java
@@ -0,0 +1,25 @@
+package com.example.repository;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import com.example.entities.CategoriaIngreso;
+
+@Repository
+public class CategoriaIngresosRepository {
+
+    @Autowired
+    private CategoriaIngresosCRUDRepository categoriaIngresosCRUDRepository;
+
+    public CategoriaIngreso save(CategoriaIngreso ci){
+        return categoriaIngresosCRUDRepository.save(ci);
+    }
+
+    public List<CategoriaIngreso> getCategoriasIngresos(){
+        return (List<CategoriaIngreso>) categoriaIngresosCRUDRepository.findAll();
+    }
+
+    public CategoriaIngreso getCategoriaByDescripcion(String desc){
+        return categoriaIngresosCRUDRepository.findByDescripcion(desc);
+    }
+}
---
+++ src/main/java/com/example/service/CategoriaIngresoService.java
@@ -0,0 +1,37 @@
+package com.example.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.entities.CategoriaIngreso;
+import com.example.entities.Usuario;
+import com.example.repository.CategoriaIngresosRepository;
+import jakarta.transaction.Transactional;
+
+@Service
+public class CategoriaIngresoService {
+    
+    @Autowired
+    private CategoriaIngresosRepository catInRepo;
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    public List<CategoriaIngreso> getCategoriasIngresos(){
+        return catInRepo.getCategoriasIngresos();
+    }
+
+    @Transactional
+    public CategoriaIngreso save(CategoriaIngreso catI){
+        return catInRepo.save(catI);
+    }
+
+    public CategoriaIngreso getCategoriaByDescripcion(String desc){
+        return catInRepo.getCategoriaByDescripcion(desc);
+    }
+    
+    public List<CategoriaIngreso> getUserCatIngresos(Long user_id){
+        Usuario usuario = usuarioService.getUsuarioById(user_id);
+        return usuario.getCategoriasIngreso();
+    }
+}
