f32a444f612e6999dd775ed7480bdd245b4bfe78
FranzRincon <franzrihe@gmail.com>
2024-04-24 20:34:38 -0500

    Agregar funcionalidad del enter al login

--- src/main/resources/static/acceso/login/js/scriptLogin.js
+++ src/main/resources/static/acceso/login/js/scriptLogin.js
@@ -1,71 +1,79 @@
-$(document).ready(function () {
-    $('#loginBtn').click(function (e) {
-        e.preventDefault();
-
-        var username = $('#username').val().trim();
-        var password = $('#password').val().trim();
-
-        if (username === "" || password === "") {
-            Swal.fire({
-                icon: "error",
-                title: "Oops...",
-                text: "Por favor, rellena todos los campos.",
-              }); 
-            return;
+$(document).ready(function() {
+    // Escuchar el evento 'keypress' en el campo de contraseña
+    $('#password').keypress(function(event) {
+        // Verificar si la tecla presionada es 'Enter' (código 13)
+        if (event.which === 13) {
+            // Llamar a la función logIn para iniciar sesión
+            logIn();
         }
+    });
+});
 
-        var data = {
-            username: username,
-            password: password
-        };
+function logIn() {
 
-        $.ajax({
-            type: "POST",
-            url: "http://localhost:8080/auth/login",
-            data: JSON.stringify(data),
-            contentType: "application/json",
-            success: function (response) {
-                localStorage.setItem('token', response.token);
-    
-                // Decodificar el token JWT
-                var tokenParts = response.token.split('.'); // Dividir el token en sus partes
-                var tokenPayload = JSON.parse(atob(tokenParts[1])); // Decodificar y analizar la parte codificada del token
-                
-                // Comprobación adicional para asegurarse de que el campo userId existe
-                if (tokenPayload.userId) {
-                    var userId = tokenPayload.userId;
-                    var rol = tokenPayload.rol;
-                    // Guardar el ID del usuario en localStorage
-                    localStorage.setItem('userId', userId);
-                    localStorage.setItem('rol',rol)
-                    // Redireccionar a la página principal
-                    if (rol == "USER"){
-                        window.location.href = '/src/main/resources/static/estudiante/principal/indexEst.html';
-                    } else {
-                        window.location.href = '/src/main/resources/static/administrador/principal/indexAdmin.html';
-                    }
-                    
+    var username = $('#username').val().trim();
+    var password = $('#password').val().trim();
+
+    if (username === "" || password === "") {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Por favor, rellena todos los campos.",
+        });
+        return;
+    }
+
+    var data = {
+        username: username,
+        password: password
+    };
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/auth/login",
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        success: function (response) {
+            localStorage.setItem('token', response.token);
+
+            // Decodificar el token JWT
+            var tokenParts = response.token.split('.'); // Dividir el token en sus partes
+            var tokenPayload = JSON.parse(atob(tokenParts[1])); // Decodificar y analizar la parte codificada del token
+
+            // Comprobación adicional para asegurarse de que el campo userId existe
+            if (tokenPayload.userId) {
+                var userId = tokenPayload.userId;
+                var rol = tokenPayload.rol;
+                // Guardar el ID del usuario en localStorage
+                localStorage.setItem('userId', userId);
+                localStorage.setItem('rol', rol)
+                // Redireccionar a la página principal
+                if (rol == "USER") {
+                    window.location.href = '/src/main/resources/static/estudiante/principal/indexEst.html';
                 } else {
-                    // Manejar el caso de que userId no exista o sea null
-                    Swal.fire({
-                        icon: "error",
-                        title: "Oops...",
-                        text: "Error al obtener la información del usuario. Por favor, intenta nuevamente.",
-                      }); 
+                    window.location.href = '/src/main/resources/static/administrador/principal/indexAdmin.html';
                 }
-            },
-            error: function (xhr, status, error) {
 
+            } else {
+                // Manejar el caso de que userId no exista o sea null
                 Swal.fire({
                     icon: "error",
                     title: "Oops...",
-                    text: "Error en el inicio de sesión: " + xhr.responseText
-                  }); 
+                    text: "Error al obtener la información del usuario. Por favor, intenta nuevamente.",
+                });
             }
-        });
-    });
+        },
+        error: function (xhr, status, error) {
 
-    $('#registrarseBtn').click(function () {
-        window.location.href = 'http://127.0.0.1:5500/src/main/resources/static/acceso/registroUsuario/regitroUsuario.html'; 
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Error en el inicio de sesión: " + xhr.responseText
+            });
+        }
     });
-});
+}
+
+function register(){
+    window.location.href = 'http://127.0.0.1:5500/src/main/resources/static/acceso/registroUsuario/regitroUsuario.html'; 
+}
